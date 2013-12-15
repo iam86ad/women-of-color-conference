79f35a6eb9fae6e4e7833e181cb4130490fda489
Anthony Daniel II <anthony@handsomegeni.us>
2013-12-15 11:56:39 -0500

    Improved markup using tables for Schedule module - No WordPress loop applied yet

--- inc/content/schedule-module.php
+++ inc/content/schedule-module.php
@@ -6,34 +6,32 @@
     <div class="container">
         <div class="row">
             <h2 class=" sect-title">Schedule</h2>
-            <article class="schedule--col">
+
+            <article class="full-width">
                 <h3>Wednesday</h3>
-                    <ul class="schedule--time-menu">
-                        <li><a href="#wed-am" data-toggle="tab">AM</a></li>
-                        <li><a href="#wed-pm" data-toggle="tab">PM</a></li>
-                    </ul>
-                    <div class="schedule--time-content">
-                        <div class="schedule--time-pane active" id="wed-am">
-                            <ul class="schedule--time-row">
-                                <li class="schedule--time">10am - 11am</li>
-                                <li class="schedule--time-details">
+                <ul class="schedule--time-menu">
+                    <li class="active"><a href="#wed-am" data-toggle="tab">AM</a></li>
+                    <li><a href="#wed-pm" data-toggle="tab">PM</a></li>
+                </ul>
+
+                <div class="schedule--time-data">
+                    <div id="wed-am" class="schedule--time-pane active">
+                        <table class="schedule--time-data">
+                            <tr>
+                                <td class="schedule--time">10am - 10:15</td>
+                                <td class="schedule--time-datails">
                                     <dl>
-                                        <dt>Workshop A</dt>
+                                        <dt>Title goes here</dt>
                                         <dd>
-                                            Nulla sit amet auctor metus, nec condimentum odio. Cras sed lorem in odio fermentum dapibus quis id libero. Nam venenatis est sed.
+                                            Donec tempor fringilla dui, non egestas magna laoreet porttitor. Maecenas condimentum lectus ac felis tincidunt, a egestas neque massa nunc.
                                         </dd>
                                     </dl>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="schedule--time-pane" id="wed-pm">
-                            I also have tab content
-                        </div>
+                                </td>
+                            </tr>
+                        </table>
                     </div>
+                </div>
             </article>
-            <footer class="action-block full-width col-md-12">
-                <a href="#" class="btn btn-primary">Great! Where is it located?</a>
-            </footer>
         </div>
     </div>
 </section>
